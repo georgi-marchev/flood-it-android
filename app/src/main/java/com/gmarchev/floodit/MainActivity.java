930ec180ef8cebad0d582619594b96ae62ea81fe
georgi-marchev <george_85_bg@yahoo.com>
2025-11-08 21:43:49 +0200

    Init project

---
+++ app/src/main/java/com/gmarchev/floodit/MainActivity.java
@@ -0,0 +1,17 @@
+package com.gmarchev.floodit;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
